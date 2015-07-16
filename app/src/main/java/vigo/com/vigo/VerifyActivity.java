acc5bef7e6401182980f7d4a37af983e9e47393b
theayryan <bhattacharya.aayush@gmail.com>
2015-07-16 23:22:42 +0530

    Changes

--- app/src/main/java/vigo/com/vigo/VerifyActivity.java
+++ app/src/main/java/vigo/com/vigo/VerifyActivity.java
@@ -3,7 +3,9 @@ package vigo.com.vigo;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,15 +31,21 @@ public class VerifyActivity extends Activity {
     private SharedPreferences preferences;
     private VigoApi otpApi;
     private SharedPreferences pref;
+    private Typeface mButtonFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.otp_verification_layout);
         TextView mHeading = (TextView) findViewById(R.id.verify_otp);
-        TextView mOtp = (TextView) findViewById(R.id.otp);
         final EditText mUserEnteredOtp = (EditText) findViewById(R.id.otp_user_entered);
         Button mVerify = (Button) findViewById(R.id.verify_button);
+        Typeface mCabin = Typeface.createFromAsset(getAssets(), "fonts/Cabin-Regular.ttf");
+        Typeface mComfortaa = Typeface.createFromAsset(getAssets(), "fonts/Comfortaa-Regular.ttf");
+        mButtonFont = Typeface.createFromAsset(getAssets(), "fonts/Button_Font.ttf");
+        mVerify.setTypeface(mButtonFont);
+        mUserEnteredOtp.setTypeface(mComfortaa);
+        mHeading.setTypeface(mCabin);
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(Constants.BASE_URL)
                 .build();
@@ -46,50 +54,54 @@ public class VerifyActivity extends Activity {
         mVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                otpApi.verifyOtp(
-                        mUserEnteredOtp.getText().toString(),
-                        pref.getString(Constants.AUTH_TOKEN, ""),
-                        new Callback<Response>() {
-                            @Override
-                            public void success(Response response, Response response2) {
-                                BufferedReader reader = null;
-                                StringBuilder sb = new StringBuilder();
-                                try {
-
-                                    reader = new BufferedReader(new InputStreamReader(response.getBody().in()));
-
-                                    String line;
-
+                if (!TextUtils.isEmpty(mUserEnteredOtp.getText().toString())) {
+                    otpApi.verifyOtp(
+                            mUserEnteredOtp.getText().toString(),
+                            pref.getString(Constants.AUTH_TOKEN, ""),
+                            new Callback<Response>() {
+                                @Override
+                                public void success(Response response, Response response2) {
+                                    BufferedReader reader = null;
+                                    StringBuilder sb = new StringBuilder();
                                     try {
-                                        while ((line = reader.readLine()) != null) {
-                                            sb.append(line);
+
+                                        reader = new BufferedReader(new InputStreamReader(response.getBody().in()));
+
+                                        String line;
+
+                                        try {
+                                            while ((line = reader.readLine()) != null) {
+                                                sb.append(line);
+                                            }
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
                                         }
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
+
+
+                                    String result = sb.toString();
+                                    Log.d("Response", result);
+                                    if (!result.contains("NOT_VERIFIED")) {
+                                        Log.d("RECIEVED", "positive");
+                                        Toast.makeText(getApplicationContext(), "Your Number Has Been Verified", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Your Number Could Not Get Verified Please Try Again Later", Toast.LENGTH_LONG).show();
+                                    }
+                                    Intent intent = new Intent(VerifyActivity.this, MainActivity.class);
+                                    startActivity(intent);
                                 }
 
-
-                                String result = sb.toString();
-                                Log.d("Response", result);
-                                if (!result.contains("NOT_VERIFIED")) {
-                                    Log.d("RECIEVED", "positive");
-                                    Toast.makeText(getApplicationContext(), "Your Number Has Been Verified", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Your Number Could Not Get Verified Please Try Again Later", Toast.LENGTH_LONG).show();
+                                @Override
+                                public void failure(RetrofitError error) {
+                                    error.printStackTrace();
                                 }
-                                Intent intent = new Intent(VerifyActivity.this, MainActivity.class);
-                                startActivity(intent);
                             }
-
-                            @Override
-                            public void failure(RetrofitError error) {
-                                error.printStackTrace();
-                            }
-                        }
-                );
+                    );
+                } else {
+                    Toast.makeText(VerifyActivity.this, "You Haven't Entered Any OTP", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
